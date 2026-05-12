68fe82f18879a096ebd959a5041f7a49f224f77d
Ivannpy <dev@puapc.net>
2026-05-12 15:28:43 -0600

    Inicia proyecto react y sirve landing page en html

---
+++ tradinn/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+
+  return (
+      <iframe
+          src="/landing_page.html"
+          title="Landing Page"
+          style={{
+            display: 'block',
+            width: '100%',
+            height: '100vh',
+            border: 'none',
+          }}
+      />
+  )
+}
+
+export default App
